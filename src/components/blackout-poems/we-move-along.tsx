28f33be57749302d5c85dcfafce823914d2fd6f1
Truman Shuck <hello@trumanshuck.com>
2019-09-12 21:58:16 -0500

    Clean up some mdx transition stuff

--- src/components/blackout-poems/we-move-along.tsx
+++ src/components/blackout-poems/we-move-along.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react'
 import Draw from './Draw'
-import PoemsLayout from '../../layouts/PoemsLayout'
 
 class WeMoveAlong extends Draw {
   private we: React.RefObject<HTMLSpanElement> = React.createRef()
@@ -106,86 +105,84 @@ class WeMoveAlong extends Draw {
   public render() {
     const { Line, Page, SVGContainer, Word } = this
     return (
-      <PoemsLayout>
-        <SVGContainer ref={this.svg}>
-          <Page bg="#555555">
-            <Line indent>
-              <Word ref={this.we}>We</Word> don’t sit on our asses here, he said. We <Word ref={this.move}>move</Word> on and up. And
-            </Line>
-            <Line>
-              wh<Word ref={this.a}>a</Word>t are your <Word ref={this.long}>long</Word>-range plans?
-            </Line>
-            <Line indent>
-              I don’t know. I suppose I just came here to be a teacher<Word ref={this.comma}>,</Word> I said.
-            </Line>
-            <Line indent>
-              He <Word ref={this.shook}>shook</Word> his head, couldn’t understand my lack of ambition. I
-            </Line>
-            <Line>wasn’t dynamic enough. Because of him those four teachers taking</Line>
-            <Line>courses were moving on and up and out. That’s what he said. Why</Line>
-            <Line>
-              should they spend their lives in the classroom <Word ref={this.with}>with</Word> kids when they
-            </Line>
-            <Line>
-              could travel the corridors of <Word ref={this.power}>power</Word>?
-            </Line>
-            <Line indent>
-              <Word ref={this.i}>I</Word> felt brave for a moment and asked him, If everyone moved on
-            </Line>
-            <Line>and up and out who would teach the children?</Line>
-            <Line indent>
-              He ignored me, <Word ref={this.allowed}>allowed</Word> himself <Word ref={this.aLittle}>a little smile</Word> with a mouth
-              that
-            </Line>
-            <Line>had no lips.</Line>
-            <Line indent>
-              I l<Word ref={this.a2}>a</Word>sted one term, September to January, before he forced me out.
-            </Line>
-            <Line>
-              It may have been the matter of the <Word ref={this.shoelace}>shoelace</Word>
-              and the <Word ref={this.rolled}>rolled-up</Word> mag-
-            </Line>
-            <Line>azine or it may have been my lack of dynamism and ambition. Still,</Line>
-            <Line>he praised me at a department meeting for my lesson on the parts of</Line>
-            <Line>
-              a sentence where I used a ballpoint pen as a visual aid<Word ref={this.period}>.</Word>
-            </Line>
-            <Line indent>This is the plastic tube that holds the ink. If you removed this tube</Line>
-            <Line>from the pen what would happen?</Line>
-            <Line indent>
-              My students look at me as if they can’t believe <Word ref={this.imAsking}>I’m asking</Word> such a
-            </Line>
-            <Line>dumb-ass question. Man, you wouldn’t be able to write.</Line>
-            <Line indent>
-              OK. Now <Word ref={this.whatIs}>what is this</Word> I’m holding in my hand?
-            </Line>
-            <Line indent>
-              Again the <Word ref={this.patient}>patient</Word> look. That’s a spring, man.
-            </Line>
-            <Line indent>And what would happen if we removed the spring?</Line>
-            <Line indent>When you try to push the tube out it won’t write because there’s</Line>
-            <Line>
-              no spring to push it and keep its <Word ref={this.littleNose}>little nose</Word> out there where all the
-            </Line>
-            <Line>writing is done and then you get in a lotta trouble because you can’t</Line>
-            <Line>
-              write your homework and the teacher’s gonna think <Word ref={this.youreCrazy}>you’re crazy</Word> if
-            </Line>
-            <Line>
-              you come in telling him about missing springs or tubes<Word ref={this.period2}>.</Word>
-            </Line>
-            <Line indent>Now look at what I’m writing on the board. “The spring makes</Line>
-            <Line>
-              the pen work.” <Word ref={this.what}>What</Word> is the subject of this sentence? In other words
-              <Word ref={this.comma2}>,</Word>
-            </Line>
-            <Line>
-              <Word ref={this.whatAre}>what are we</Word> talking about in this sentence<Word ref={this.qmark}>?</Word>
-            </Line>
-            <Line indent>The pen.</Line>
-          </Page>
-        </SVGContainer>
-      </PoemsLayout>
+      <SVGContainer ref={this.svg}>
+        <Page bg="#555555">
+          <Line indent>
+            <Word ref={this.we}>We</Word> don’t sit on our asses here, he said. We <Word ref={this.move}>move</Word> on and up. And
+          </Line>
+          <Line>
+            wh<Word ref={this.a}>a</Word>t are your <Word ref={this.long}>long</Word>-range plans?
+          </Line>
+          <Line indent>
+            I don’t know. I suppose I just came here to be a teacher<Word ref={this.comma}>,</Word> I said.
+          </Line>
+          <Line indent>
+            He <Word ref={this.shook}>shook</Word> his head, couldn’t understand my lack of ambition. I
+          </Line>
+          <Line>wasn’t dynamic enough. Because of him those four teachers taking</Line>
+          <Line>courses were moving on and up and out. That’s what he said. Why</Line>
+          <Line>
+            should they spend their lives in the classroom <Word ref={this.with}>with</Word> kids when they
+          </Line>
+          <Line>
+            could travel the corridors of <Word ref={this.power}>power</Word>?
+          </Line>
+          <Line indent>
+            <Word ref={this.i}>I</Word> felt brave for a moment and asked him, If everyone moved on
+          </Line>
+          <Line>and up and out who would teach the children?</Line>
+          <Line indent>
+            He ignored me, <Word ref={this.allowed}>allowed</Word> himself <Word ref={this.aLittle}>a little smile</Word> with a mouth
+            that
+          </Line>
+          <Line>had no lips.</Line>
+          <Line indent>
+            I l<Word ref={this.a2}>a</Word>sted one term, September to January, before he forced me out.
+          </Line>
+          <Line>
+            It may have been the matter of the <Word ref={this.shoelace}>shoelace</Word>
+            and the <Word ref={this.rolled}>rolled-up</Word> mag-
+          </Line>
+          <Line>azine or it may have been my lack of dynamism and ambition. Still,</Line>
+          <Line>he praised me at a department meeting for my lesson on the parts of</Line>
+          <Line>
+            a sentence where I used a ballpoint pen as a visual aid<Word ref={this.period}>.</Word>
+          </Line>
+          <Line indent>This is the plastic tube that holds the ink. If you removed this tube</Line>
+          <Line>from the pen what would happen?</Line>
+          <Line indent>
+            My students look at me as if they can’t believe <Word ref={this.imAsking}>I’m asking</Word> such a
+          </Line>
+          <Line>dumb-ass question. Man, you wouldn’t be able to write.</Line>
+          <Line indent>
+            OK. Now <Word ref={this.whatIs}>what is this</Word> I’m holding in my hand?
+          </Line>
+          <Line indent>
+            Again the <Word ref={this.patient}>patient</Word> look. That’s a spring, man.
+          </Line>
+          <Line indent>And what would happen if we removed the spring?</Line>
+          <Line indent>When you try to push the tube out it won’t write because there’s</Line>
+          <Line>
+            no spring to push it and keep its <Word ref={this.littleNose}>little nose</Word> out there where all the
+          </Line>
+          <Line>writing is done and then you get in a lotta trouble because you can’t</Line>
+          <Line>
+            write your homework and the teacher’s gonna think <Word ref={this.youreCrazy}>you’re crazy</Word> if
+          </Line>
+          <Line>
+            you come in telling him about missing springs or tubes<Word ref={this.period2}>.</Word>
+          </Line>
+          <Line indent>Now look at what I’m writing on the board. “The spring makes</Line>
+          <Line>
+            the pen work.” <Word ref={this.what}>What</Word> is the subject of this sentence? In other words
+            <Word ref={this.comma2}>,</Word>
+          </Line>
+          <Line>
+            <Word ref={this.whatAre}>what are we</Word> talking about in this sentence<Word ref={this.qmark}>?</Word>
+          </Line>
+          <Line indent>The pen.</Line>
+        </Page>
+      </SVGContainer>
     )
   }
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,88 +1,18 @@
 import React, { useState } from 'react';
-import { StaticQuery, graphql, Link } from 'gatsby'
 import styled from '@emotion/styled'
 
-import Page from '../components/Page'
-import Container from '../components/Container'
+import { Container, Page, PreviewItems, Tags } from '../components'
 import { IndexLayout } from '../layouts'
-import { device, dimensions, colors } from '../styles/variables'
+import { colors } from '../styles/variables'
 
 export const Splash = styled.h1`
   color: ${colors.brand};
   text-align: center;
-  margin-top: ${dimensions.margins.xl}rem;
-  text-align: center;
-
-  @media ${device.xs} {
-    font-size: ${dimensions.headingSizes.splashSm}rem;
-  }
-
-  @media ${device.md} {
-    font-size: ${dimensions.headingSizes.splashSm + 5}rem;
-  }
-
-  @media ${device.lg} {
-    font-size: ${dimensions.headingSizes.splash}rem;
-  }
+  font-size: 5rem;
 `
-
-const tagsQuery = graphql`
-  query TagsQuery {
-    allMdx {
-      group(field: frontmatter___tags) {
-        tag: fieldValue
-        totalCount
-      }
-    }
-  }
-`
-
-const pageQuery = graphql`
-  query IndexPageQuery {
-    allMdx(sort: {fields: frontmatter___date}) {
-      nodes {
-        frontmatter {
-          title
-          date
-          tags
-        }
-        fields {
-          slug
-        }
-      }
-    }
-  }
-`
-
-const calculateTags = (tags, clickedTag) => {
-  if(tags.includes(clickedTag)) {
-    return tags.filter(t => t !== clickedTag)
-  }
-
-  return [...tags, clickedTag]
-}
 
 const IndexPage = () => {
-  const [tags, setTags] = useState([]);
-
-  const Tags: React.FC = (data) => {
-    return data.allMdx.group.map(g => {
-      const onClick = () => setTags(calculateTags(tags, g.tag))
-      const isToggled = tags.includes(g.tag) ? 'on' : 'off'
-      return <p onClick={onClick}>{g.tag} {isToggled}</p>
-    })
-  }
-
-  const Content: React.FC = (data) => {
-    return data.allMdx.nodes.map(node => {
-      const nodeTags = new Set(node.frontmatter.tags)
-      const tagsIncluded = tags.filter(t => nodeTags.has(t)).length === 0
-      if(tags.length > 0 && tagsIncluded) {
-        return null
-      }
-      return <Link to={node.fields.slug}>{node.frontmatter.title}</Link>
-    }).filter(i => !!i)
-  }
+  const [tags, setTags] = useState<string[]>([]);
 
   return <IndexLayout>
     <Page>
@@ -90,10 +20,10 @@ const IndexPage = () => {
         <Splash>Hello</Splash>
       </Container>
       <Container>
-        <StaticQuery query={tagsQuery} render={Tags} />
+        <Tags tags={tags} setTags={setTags} />
       </Container>
       <Container>
-        <StaticQuery query={pageQuery} render={Content} />
+        <PreviewItems tags={tags} />
       </Container>
     </Page>
   </IndexLayout>
