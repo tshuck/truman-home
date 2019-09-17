12c110170753cba267ad7da16e966da1fb4c99b8
Truman Shuck <hello@trumanshuck.com>
2019-09-16 23:26:23 -0500

    Add back button

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -4,8 +4,8 @@ import { Link } from 'gatsby'
 import Container from '../components/Container'
 import Layout from '../layouts'
 
-const NotFoundPage = () => (
-  <Layout>
+const NotFoundPage = ({location}: {location: object}) => (
+  <Layout location={location}>
     <Container>
       <h1>404: Page not found.</h1>
       <p>
