3a8c7e726df7f2cc112a62f637dd2f07725e5492
Truman Shuck <truman@hatchloyalty.com>
2019-09-16 22:39:17 -0500

    Default tag checkboxes on; move tag query up to page index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,24 +1,48 @@
 import React, { useState } from 'react'
 import { Heading } from 'rebass'
-
+import { useStaticQuery, graphql } from 'gatsby'
 import { Container, PreviewItems, Tags } from '../components'
 import Layout from '../layouts'
 
+interface Group {
+  tag: string
+}
+interface Data {
+  allMdx: {
+    group: Group[]
+  }
+}
+
+const query = graphql`
+  query TagsQuery {
+    allMdx {
+      group(field: frontmatter___tags) {
+        tag: fieldValue
+        totalCount
+      }
+    }
+  }
+`
+
 const IndexPage = () => {
-  const [tags, setTags] = useState<string[]>([])
+  const {
+    allMdx: { group }
+  }: Data = useStaticQuery(query)
+  const availableTags = group.map(g => g.tag)
+  const [selectedTags, setSelectedTags] = useState<string[]>(availableTags)
 
   return (
     <Layout>
       <Container>
-        <Heading textAlign="center" fontSize={8} color="white" m={5}>
+        <Heading textAlign="center" fontSize={[6,8]} color="white" m={5}>
           Hello
         </Heading>
       </Container>
       <Container>
-        <Tags tags={tags} setTags={setTags} />
+        <Tags availableTags={availableTags} selectedTags={selectedTags} setSelectedTags={setSelectedTags} />
       </Container>
       <Container>
-        <PreviewItems tags={tags} />
+        <PreviewItems selectedTags={selectedTags} />
       </Container>
     </Layout>
   )
