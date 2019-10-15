0df120699d20ef6d30372d4026344e1f5a011572
Truman Shuck <hello@trumanshuck.com>
2019-10-14 22:33:46 -0500

    Minor edits

--- src/components/Footnote.tsx
+++ src/components/Footnote.tsx
@@ -4,7 +4,7 @@ import { Link, LinkProps } from 'rebass'
 export default (props: LinkProps) => {
   const { children, ...rest } = props
   return (
-    <Link {...rest} sx={{ verticalAlign: 'super', textDecoration: 'none' }} fontSize={1} color="muted">
+    <Link {...rest} sx={{ verticalAlign: 'super' }} fontSize={1} color="accent">
       {children}
     </Link>
   )
